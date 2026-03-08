347ee90fa7f878a62aa7d1e6e724920220e49a5f
mhmleo <leoalanandrade@gmail.com>
2026-03-08 19:34:51 -0400

    first sproj commitment thank you Kash you're the best!!!!

---
+++ Untitled.R
@@ -0,0 +1,9 @@
+## Script for RCA Analysis
+
+#Load packagses 
+library(ggplot2) # Used visualizations
+library(readxl) # Used to read excel Files
+library(dplyr) # Used to tidy up datasets
+library(arrow) # Used to read parquet files if necessary 
+library(janitor) # Used to clean names
+library(readr) # Used to read 
